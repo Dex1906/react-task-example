eaa9082f50e23e3238d0a213480ce6ee46b818d7
Lalo <eduardguerrero19@hotmail.com>
2024-04-04 13:15:36 -0600

    My first commit

---
+++ src/data/tasks.js
@@ -0,0 +1,17 @@
+export const tasks = [
+  {
+    id: 0,
+    title: "Mi primer tarea",
+    description: "Elaborar un programa",
+  },
+  {
+    id: 1,
+    title: "Mi segunda tarea",
+    description: "Elaborar un programa 1",
+  },
+  {
+    id: 2,
+    title: "Mi tercera tarea",
+    description: "Elaborar un programa 2",
+  },
+];
